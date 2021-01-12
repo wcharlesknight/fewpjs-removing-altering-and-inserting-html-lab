c9268720cac55d40da03a764dda4c811511a9fec
Charlie Knight <williamchknight@gmail.com>
2021-01-11 18:49:07 -0800

    done

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.querySelector("main#main").remove()
+
+let newHeader = document.createElement('h1')
+newHeader.className = 'victory'
+newHeader.innerHTML = 'charlie is the champion'
