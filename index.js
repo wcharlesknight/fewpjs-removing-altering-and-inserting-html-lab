d933d64097c1f5cbdb9d651d3d1c34739075eca8
Charlie Knight <williamchknight@gmail.com>
2021-01-11 18:48:02 -0800

    commit tree 66ac56862d8575592f7ea58b5f839a5ccce947cb

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.querySelector("main#main").remove()
+
+let newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = 'charlie is the champion'
